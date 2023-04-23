fd4905c1ab75c52bbce28029e989a91daa27a281
Clemens Nasenberg <clemens@nasenberg.eu>
2023-04-23 16:23:42 +0200

    Add reset for all flops

--- src/clemensnasenberg_top.v
+++ src/clemensnasenberg_top.v
@@ -30,31 +30,40 @@ module clemensnasenberg_top  #(
     assign xor_data_right = ^data_right;
 
     always @ (posedge sck) begin
-        if (wsp == 1'b1) begin
-            control_reg[CTRL_WIDTH-2:0] <= 'b0;
-            control_reg[CTRL_WIDTH-1] <= 1'b1;
-            data[WIDTH-2:0] <= 'b0;
-            data[WIDTH-1] <= sd;
+        if (reset == 1'b1) begin
+            wsd <= 1'b0;
+            wsd <= 1'b0;
+            data_left <= 'b0;
+            data_right <= 'b0;
+            data <= 'b0;
+            control_reg <= 'b0;
         end else begin
-            control_reg[CTRL_WIDTH-1] <= 1'b0;
-            for (i = 1; i < CTRL_WIDTH; i = i+1) begin
-                control_reg[CTRL_WIDTH-1-i] <= control_reg[CTRL_WIDTH-i];
-            end
-            for (i = 0; i <= CTRL_WIDTH; i = i+1) begin
-                if (control_reg[CTRL_WIDTH-i] == 1'b1) begin        
-                    data[WIDTH-1-i] <= sd;
+            if (wsp == 1'b1) begin
+                control_reg[CTRL_WIDTH-2:0] <= 'b0;
+                control_reg[CTRL_WIDTH-1] <= 1'b1;
+                data[WIDTH-2:0] <= 'b0;
+                data[WIDTH-1] <= sd;
+            end else begin
+                control_reg[CTRL_WIDTH-1] <= 1'b0;
+                for (i = 1; i < CTRL_WIDTH; i = i+1) begin
+                    control_reg[CTRL_WIDTH-1-i] <= control_reg[CTRL_WIDTH-i];
+                end
+                for (i = 0; i <= CTRL_WIDTH; i = i+1) begin
+                    if (control_reg[CTRL_WIDTH-i] == 1'b1) begin        
+                        data[WIDTH-1-i] <= sd;
+                    end
                 end
             end
-        end
     
-        wsd <= ws;
-        wsd_reg <= wsd;
-        
-        if (!wsd & wsp) begin
-            data_left <= data;
-        end 
-        if (wsd & wsp) begin
-            data_right <= data;
+            wsd <= ws;
+            wsd_reg <= wsd;
+            
+            if (!wsd & wsp) begin
+                data_left <= data;
+            end 
+            if (wsd & wsp) begin
+                data_right <= data;
+            end
         end
     end
 
@@ -62,14 +71,18 @@ module clemensnasenberg_top  #(
 
     assign sd_out = data_shift[WIDTH-1];
     always @ (negedge sck) begin
-        if (wsp == 1'b1) begin
-            if (wsd == 1'b1) begin
-                data_shift <= data_right;
-            end else begin
-                data_shift <= data_left;
-            end
+        if (reset == 1'b1) begin
+            data_shift <= 'b0;
         end else begin
-            data_shift <= {data_shift[WIDTH-2:0], 1'b0};
+            if (wsp == 1'b1) begin
+                if (wsd == 1'b1) begin
+                    data_shift <= data_right;
+                end else begin
+                    data_shift <= data_left;
+                end
+            end else begin
+                data_shift <= {data_shift[WIDTH-2:0], 1'b0};
+            end
         end
     end
 endmodule
